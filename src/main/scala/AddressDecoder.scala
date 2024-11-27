766f2ea6f14808a98caae6b00048c030e92d284f
nickrallison <nickrallison1@gmail.com>
2024-11-27 00:22:51 -0700

    Made started class

--- src/main/scala/AddressDecoder.scala
+++ src/main/scala/AddressDecoder.scala
@@ -3,4 +3,19 @@ package tech.rocksavage.chiselware.AddressDecoder
 import chisel3._
 import chisel3.util._
 
-class AddressDecoder {}
+class AddressDecoder(decodeMap: Vector[Addressable]) {
+
+  // val decodeMap: Vec[Addressable]
+
+  def decode(address: UInt, data: UInt): Unit = {
+    var count: Int = 0;
+    // iterate through addressable and increment count each time, if address - count is less than or equal to size, then call closure and break
+    for (addressable <- decodeMap) {
+      when(address - count.U <= addressable.size.U) {
+        addressable.closure(address - count.U, data)
+        return
+      }
+      count += addressable.size
+    }
+  }
+}
--- src/main/scala/AddressableModule.scala
+++ src/main/scala/AddressableModule.scala
@@ -3,4 +3,6 @@ package tech.rocksavage.chiselware.AddressDecoder
 import chisel3._
 import chisel3.util._
 
-class Addressable(val size: Int, val closure: (UInt, UInt) => Unit) {}
+trait AddressableModule extends Module {
+  val addressDecoder: AddressDecoder
+}
