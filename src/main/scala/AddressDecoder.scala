4edeb224f27151d4dd76cf7865c0451b1cc4d7cd
nickrallison <nickrallison1@gmail.com>
2024-11-27 21:00:40 -0700

    added regs inside decoder

--- src/main/scala/AddressDecoder.scala
+++ src/main/scala/AddressDecoder.scala
@@ -6,13 +6,18 @@ import chisel3._
 import chisel3.util._
 
 // Source: https://github.com/chipsalliance/rocket-chip/issues/1668#issuecomment-433528365
-class AddrDecoder[T <: Data with Num[T]](dType: T, n: Int) extends Module {
+class AddrDecoder(
+    p: BaseParams,
+    n: Int,
+    numRegs: Int
+) extends Module {
   val io = IO(new Bundle {
-    val range0 = Input(Vec(n, dType))
-    val range1 = Input(Vec(n, dType))
-    val addr   = Input(dType)
+    val range0 = Input(Vec(n, p.addressWidth))
+    val range1 = Input(Vec(n, p.addressWidth))
+    val addr   = Input(addressWidth)
     val en     = Input(Bool())
     val sel    = Output(Vec(n, Bool()))
+    val regs   = Vec(numRegs, Output(p.dataWidth))
   })
   // Curried function which accepts a tuple and an input addr
   // Use map to apply it to inputs
@@ -23,8 +28,19 @@ class AddrDecoder[T <: Data with Num[T]](dType: T, n: Int) extends Module {
   for (i <- 0 until n) {
     io.sel(i) := false.B
   }
+  val effectiveRange0 = io.range0 map (_ + numRegs.U)
+  val effectiveRange1 = io.range1 map (_ + numRegs.U)
+
   when(io.en) {
-    io.sel := io.range0 zip io.range1 map (inside(_)(io.addr))
+    // if addr is in reg range, then set program those, otherwise do check for io.sek
+
+    when(inside(0, numRegs)(io.addr)) {
+      io.regs(io.addr) := io.data
+    }.otherwise {
+      for (i <- 0 until n) {
+        io.sel(i) := inside(io.range0(i), io.range1(i))(io.addr)
+      }
+    }
   }
   // $onehot0 output encoding check
   assert(PopCount(io.sel) <= 1.U, "Invalid addr decoding")
